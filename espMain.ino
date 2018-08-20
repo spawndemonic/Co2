f0aeaaea4c189268088920274cb5328b7eaac174
Spawndemonic <spawndemonic@gmail.com>
2018-08-19 22:33:29 -0400

    Small changes

--- espMain.ino
+++ espMain.ino
@@ -35,9 +35,9 @@
 
 const char *ssid = "YourSSIDHere";
 const char *password = "YourPSKHere";
-
+//set webserver to prot 80
 ESP8266WebServer server(80);
-
+//set pin for the led
 const int led = 13;
 
 void handleRoot() {
@@ -114,7 +114,6 @@ void setup(void) {
   }
 
   server.on("/", handleRoot);
-  server.on("/test.svg", drawGraph);
   server.on("/inline", []() {
     server.send(200, "text/plain", "this works as well");
   });
@@ -127,20 +126,3 @@ void loop(void) {
   server.handleClient();
 }
 
-void drawGraph() {
-  String out = "";
-  char temp[100];
-  out += "<svg xmlns=\"http://www.w3.org/2000/svg\" version=\"1.1\" width=\"400\" height=\"150\">\n";
-  out += "<rect width=\"400\" height=\"150\" fill=\"rgb(250, 230, 210)\" stroke-width=\"1\" stroke=\"rgb(0, 0, 0)\" />\n";
-  out += "<g stroke=\"black\">\n";
-  int y = rand() % 130;
-  for (int x = 10; x < 390; x += 10) {
-    int y2 = rand() % 130;
-    sprintf(temp, "<line x1=\"%d\" y1=\"%d\" x2=\"%d\" y2=\"%d\" stroke-width=\"1\" />\n", x, 140 - y, x + 10, 140 - y2);
-    out += temp;
-    y = y2;
-  }
-  out += "</g>\n</svg>\n";
-
-  server.send(200, "image/svg+xml", out);
-}
